e347759fa0d01c06f060cb913377e72368e834de
Mazen Shawky <mazenshawqy@gmail.com>
2024-09-15 21:12:18 +0300

    Solve Polo the Penguin and Segments

---
+++ src/PoloThePenguinAndSegments.java
@@ -0,0 +1,31 @@
+// https://codeforces.com/contest/289/problem/A
+
+import java.util.Scanner;
+
+public class PoloThePenguinAndSegments {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        int minNumOfMovesNeeded = 0, totalValuesOfSegments = 0;
+
+        int segmentsCount = scanner.nextInt();
+        int divisibleNum = scanner.nextInt();
+
+        for (int i = 0; i < segmentsCount; i++) {
+            int firstNum = scanner.nextInt();
+            int secondNum = scanner.nextInt();
+
+            totalValuesOfSegments += calculateValueOfSegment(firstNum, secondNum);
+        }
+
+        while (totalValuesOfSegments % divisibleNum != 0) {
+            totalValuesOfSegments++;
+            minNumOfMovesNeeded++;
+        }
+        System.out.println(minNumOfMovesNeeded);
+    }
+
+    public static int calculateValueOfSegment(int firstNum, int secondNum) {
+        return secondNum - firstNum + 1;
+    }
+}
